4ff5b53e8017383c996421166a78b383d284f9fa
Sebastian Valencia <sebastianvalencia.ceron@gmail.com>
2021-12-07 13:19:36 -0500

    Adding Spring Config. Closes #22

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/VetServiceMap.java
@@ -3,9 +3,11 @@ package guru.springframework.petclinic.services.map;
 import guru.springframework.petclinic.model.Vet;
 import guru.springframework.petclinic.services.CrudService;
 import guru.springframework.petclinic.services.VetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
