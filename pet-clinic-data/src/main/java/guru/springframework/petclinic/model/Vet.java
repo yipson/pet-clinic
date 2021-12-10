8f13b3366e9971bdb82f301d88458ac3eaa5ff30
Sebastian Valencia <sebastianvalencia.ceron@gmail.com>
2021-12-10 05:03:39 -0500

    Fixed Contact info properties to Owner. Closes #28

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Vet.java
@@ -4,34 +4,7 @@ import java.util.Set;
 
 public class Vet extends Person {
 
-    private String address;
-    private String city;
-    private String telephone;
     private Set<Speciality> specialities;
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
 
     public Set<Speciality> getSpecialities() {
         return specialities;
