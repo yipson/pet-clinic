734d43ccbfef474f1107b59c089576de75c86f1e
Sebastian Valencia <sebastianvalencia.ceron@gmail.com>
2021-12-15 12:05:29 -0500

    Added visit Entity .Closes #38

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Pet.java
@@ -2,6 +2,8 @@ package guru.springframework.petclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -22,6 +24,10 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birhtDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    //One pet could have many visits
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
