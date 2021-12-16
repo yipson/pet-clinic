c233c132be89a9d88b9054dac8bfac0845747e91
Sebastian Valencia <sebastianvalencia.ceron@gmail.com>
2021-12-16 16:57:37 -0500

    Added Project Lombok to project. Closes #51

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Pet.java
@@ -1,10 +1,17 @@
 package guru.springframework.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity {
@@ -29,35 +36,5 @@ public class Pet extends BaseEntity {
     //One pet could have many visits
     private Set<Visit> visits = new HashSet<>();
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public PetType getPetType() {
-        return petType;
-    }
-
-    public void setPetType(PetType petType) {
-        this.petType = petType;
-    }
-
-    public Owner getOwner() {
-        return owner;
-    }
-
-    public void setOwner(Owner owner) {
-        this.owner = owner;
-    }
-
-    public LocalDate getBirhtDate() {
-        return birhtDate;
-    }
-
-    public void setBirhtDate(LocalDate birhtDate) {
-        this.birhtDate = birhtDate;
-    }
 }
