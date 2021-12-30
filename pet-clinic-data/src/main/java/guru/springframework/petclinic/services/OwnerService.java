96b01e7386c4a0e67bc49de33b052ca2e54cc05e
yipson <sebastianvalencia.ceron@gmail.com>
2021-12-29 19:44:56 -0500

    Impl find owner. Closes #54

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/OwnerService.java
@@ -2,9 +2,13 @@ package guru.springframework.petclinic.services;
 
 import guru.springframework.petclinic.model.Owner;
 
+import java.util.List;
+
 
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
